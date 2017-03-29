dfbc6ed733d7b61c0807b5de535bb1db42a1a64b
Thricehappy <thricehapus@gmail.com>
2017-03-29 15:10:38 -0400

    Style fix
    Update bg color for player name for better contrast on light backgrounds

--- includes/auth-bg.php
+++ includes/auth-bg.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['auth_characterid'])) {
 	$charimg = '<img src="http://image.eveonline.com/Character/'.$_SESSION['auth_characterid'].'_64.jpg">';
 	$charname = $_SESSION['auth_charactername'];
-	$chardiv = '<div style="text-align: center;">'.$charimg.'<br /><span class="white">'.$charname.'</span><br /><span class="descr"><a href="/auth/logout.php">logout</a></span></div>';
+	$chardiv = '<div style="text-align: center;">'.$charimg.'<br /><div style="background-color: black;"><span class="white">'.$charname.'</span><br /><span class="descr"><a href="/auth/logout.php">logout</a></span></div></div>';
 }
 else {
 	$chardiv = '<a href="../auth/login.php"><img src="../img/EVE_SSO_Login_Buttons_Small_Black.png"></a>';
