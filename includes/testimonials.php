e8db1b72b6e40c346bac3abeb071eb2948c0b23e
Thricehappy <thricehapus@gmail.com>
2017-05-02 15:21:33 -0400

    HOTFIX: copy edits

--- includes/testimonials.php
+++ includes/testimonials.php
@@ -5,7 +5,7 @@
         	I expect this service will be used well by others in need."<br /><br />
 			<span class="pull-right" style="text-align: right;">
 				anonymous pilot<br />
-				<span style="font-size: 85%;">rescued from J105700 on YC119-May-01</span>
+				<span style="font-size: 85%;">rescued on YC119-May-01</span>
 			</span>
         </div>
     </div>
@@ -16,13 +16,13 @@
             "I have to admit I was very skeptical when I first read about EvE-Scout. 
             I decided that even if it was legit, I probably would never need it. 
             It turns out that a week later I logged into my alt that was camping 
-            J222120 and realized I had forgotten to retrieve my probes during the 
+            [a wormhole] and realized I had forgotten to retrieve my probes during the 
             previous session. :-| You know the rest of the story.<br /><br />
 			"Thanks for the rescue. The service you provided was courteous, 
 			business-like, and timely."<br /><br />
 			<span class="pull-right" style="text-align: right;">
 				Jenan Tahnek<br />
-				<span style="font-size: 85%;">rescued from J222120 on YC119-Apr-26</span>
+				<span style="font-size: 85%;">rescued on YC119-Apr-26</span>
 			</span>
         </div>
     </div>
