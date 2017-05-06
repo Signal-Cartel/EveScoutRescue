d9af3d610fd3450b33cbcd94aae76752df9a2583
Thricehappy <thricehapus@gmail.com>
2017-05-06 11:33:53 -0400

    UPDATE: Add new testimonial

--- includes/testimonials.php
+++ includes/testimonials.php
@@ -1,8 +1,8 @@
 <div class="item active">
 	<div class="carousel-content">
     	<div>
-        	"The service was both easy and quick, and got me out of quite a pickle.
-        	I expect this service will be used well by others in need."<br /><br />
+        	The service was both easy and quick, and got me out of quite a pickle.
+        	I expect this service will be used well by others in need.<br /><br />
 			<span class="pull-right" style="text-align: right;">
 				anonymous pilot<br />
 				<span style="font-size: 85%;">rescued on YC119-May-01</span>
@@ -13,13 +13,13 @@
 <div class="item">
 	<div class="carousel-content">
     	<div>
-            "I have to admit I was very skeptical when I first read about EvE-Scout. 
+            I have to admit I was very skeptical when I first read about EvE-Scout. 
             I decided that even if it was legit, I probably would never need it. 
             It turns out that a week later I logged into my alt that was camping 
             [a wormhole] and realized I had forgotten to retrieve my probes during the 
             previous session. :-| You know the rest of the story.<br /><br />
-			"Thanks for the rescue. The service you provided was courteous, 
-			business-like, and timely."<br /><br />
+			Thanks for the rescue. The service you provided was courteous, 
+			business-like, and timely.<br /><br />
 			<span class="pull-right" style="text-align: right;">
 				Jenan Tahnek<br />
 				<span style="font-size: 85%;">rescued on YC119-Apr-26</span>
@@ -27,4 +27,20 @@
         </div>
     </div>
 </div>
-                
+<div class="item">
+	<div class="carousel-content">
+    	<div>
+            I had doubts about Signal Cartel when I first stumbled upon their rescue cache, 
+            but I contacted them anyway. The cache got me out of the hole that confined me for 
+            over a year because of stupid mistakes that I made.<br /><br />
+            If I happen to get lost in a hole again, and find myself with a rescue cache, 
+            I'd not hesitate to contact them again. They are the only ones other than my corp, 
+            alliance, or friends that I trust with my life in a game where everyone is out to 
+            get you. I highly recommend them.<br /><br />
+			<span class="pull-right" style="text-align: right;">
+				Miss Fiery<br />
+				<span style="font-size: 85%;">rescued on YC119-Mar-20</span>
+			</span>
+        </div>
+    </div>
+</div>      
