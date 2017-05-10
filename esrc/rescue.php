47714fcfffd92c82cc0d3b228afbde68e5a07c74
bikerunner <klaus@intershop.de>
2017-05-11 00:40:27 +0200

    Extend SAR handling
    - add list of active requests
    - add link to manage page
    - merge branch 'master' into I72_RescueInfo

--- esrc/rescue.php
+++ esrc/rescue.php
@@ -75,7 +75,7 @@ if (isset($_REQUEST['action']) && $_REQUEST['action'] === 'Create')
 		<div class="row">
 			<div class="col-sm-3"></div>
 			<div class="col-sm-5" style="text-align: left;">
-				<form method="post" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+				<form method="post" action="./search.php">
 					<div class="form-group">
 						<input type="text" name="targetsystem" size="30" autoFocus="autoFocus" 
 							autocomplete="off" class="targetsystem" placeholder="System Name" 
@@ -92,15 +92,60 @@ if (isset($_REQUEST['action']) && $_REQUEST['action'] === 'Create')
 	</div>
 	<?php include_once '../includes/top-right.php'; ?>
 </div>
-<div class="ws"></div>
+<div class="ws">
 <form action="rescue.php" method="POST">
-System: <?php if (isset($targetsystem)) {?><input placeholder="System name" class="system tt-query black" type="text" autocomplete="off" name="system" value="<?=Output::htmlEncodeString($targetsystem)?>"/> <?php } else { ?> <input placeholder="System name" class="system tt-query black" type="text" name="system" class="system" autocomplete="off" /> <?php }?>
-Pilot: <input type="text" class="black" name="pilot"  /> 
-Can refit: <select class="black" name="canrefit" ><option value="0" default>No</option><option value="1">Yes</option></select> 
+<p> System: <?php if (isset($targetsystem)) {?><input placeholder="System name" class="system tt-query black" type="text" autocomplete="off" name="system" value="<?=Output::htmlEncodeString($targetsystem)?>"/> <?php } else { ?> <input placeholder="System name" class="system tt-query black" type="text" name="system" class="system" autocomplete="off" /> <?php }?>
+</p>
+<p>Pilot: <input type="text" class="black" name="pilot"  />
+</p>
+<p> 
+Can refit: <select class="black" name="canrefit" ><option value="0" default>No</option><option value="1">Yes</option></select>
+</p>
+<p> 
 Note: 					<textarea class="form-control black" id="notes" name="notes" rows="3"></textarea>
+</p>
+<p>
 Send: <input class="black" type="submit" name="action" value="Create"> 
-
+</p>
 </form>
+</div>
+<?php 
+$database->query("select id, requestdate, system, pilot, canrefit from rescuerequest where finished = 0 order by requestdate");
+// $database->execute();
+$data = $database->resultset();
+// echo "<pre>";
+// print_r($database);
+// echo "\n";
+// print_r($data);
+// echo "</pre>";
+$database->closeQuery();
+if (isset($data))
+{
+?>
+<div>
+<table width="90%">
+<tr>
+<th>Started</th><th>System</th><th>Pilot</th><th>refit</th><th>Manage</th>
+</tr>
+<?php 
+	foreach ($data as $row) {
+		echo "<tr>";
+		echo "<td>".Output::getEveDate($row['requestdate'])."</td>";
+		echo "<td>".Output::htmlEncodeString($row['system'])."</td>";
+		echo "<td>".Output::htmlEncodeString($row['pilot'])."</td>";
+		echo "<td>".Output::htmlEncodeString($row['canrefit'])."</td>";
+		echo "<td><a href=\"./rescuemanage.php?request=".$row['id']."\">Manage the request</a></td>";
+		echo "</tr>";
+	}
+}
+else
+{
+	echo "No active rescure requests";
+}
+?>
+</table>
+
+</div>
 
 
 </div>
---
+++ esrc/rescuemanage.php
@@ -0,0 +1,5 @@
+<?php 
+
+echo "Manage request ".$_REQUEST['request'];
+
+?>
