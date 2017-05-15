ed05eecb04e75e872ab853d4b4ae54bda9a948e2
Thricehappy <thricehapus@gmail.com>
2017-05-15 09:33:49 -0400

    Add Contribute page
    * Make necessary adjustments to nav and includes to accommodate new page

--- home/index.php
+++ home/index.php
@@ -55,10 +55,7 @@ include_once '../includes/auth-inc.php';
 <div class="container">
 <div class="row" id="header" style="padding-top: 10px;">
 	<?php include_once '../includes/top-left.php'; ?>
-	<div class="col-sm-8 white" style="text-align: center; height: 100px; vertical-align: middle;">
-		<br /><span class="sechead">New Eden's Premier Wormhole Rescue Service</span><br /><br />
-		Please join the in-game channel <span style="color: gold; font-weight: bold;">EvE-Scout</span> for further assistance.
-	</div>
+	<?php include_once '../includes/top-center.php'; ?>
 	<?php include_once '../includes/top-right.php'; ?>
 </div>
 <?php
@@ -136,32 +133,6 @@ $ctractive = $caches->getActiveCount();
 			</div>
 			<div class="panel-body">
 				<p class="lead">If we don't have a rescue cache in your current wormhole system, don't despair! Our Search and Rescue pilots will work hard to find you and scout you back to known space.</p>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="ws"></div>
-<div class="row">
-	<div class="col-sm-12">
-		<div class="panel panel-default">
-			<div class="panel-body">
-				<p><a href="http://www.eve-scout.com/signal-cartel/">Signal Cartel</a>, 
-					the core corporation of the 
-					<a href="https://gate.eveonline.com/Alliance/EvE-Scout%20Enclave">EvE-Scout 
-					Enclave</a> alliance, is a neutral, non-profit entity that aims to 
-					provide a valuable public service to all of New Eden. As such, one 
-					of our primary initiatives is to look for and rescue capsuleers who 
-					are stranded inside wormholes without equipment to get out by themselves. 
-					In accordance to our Credo, our services are free and available to 
-					capsuleers of all play styles and allegiance.</p>
-				<p style="text-align: center;">
-			        <a class="btn btn-primary btn-lg" href="about.php" role="button">
-			        	Learn More About Us</a>
-			    </p>
-				<p>If you also think that no one should be stranded inside a wormhole due 
-					to server problems or socket disconnects, please support this initiative 
-					by not blowing up our rescue caches! We sincerely thank you for your 
-					cooperation!</p>
 			</div>
 		</div>
 	</div>
---
+++ includes/top-center.php
@@ -0,0 +1,5 @@
+<div class="col-sm-8 white" style="text-align: center;">
+	<span class="sechead">New Eden's Premier Wormhole Rescue Service</span><br /><br />
+	<a class="btn btn-primary btn-md" href="about.php" role="button">About Us</a>&nbsp;&nbsp;&nbsp;&nbsp;
+	<a class="btn btn-primary btn-md" href="donate.php" role="button">Contribute</a>
+</div>
