39254952ac4562091213238e02a1ea610369f4d0
Thricehappy <thricehapus@gmail.com>
2017-06-08 08:29:27 -0400

    Update stats for this week's contest

--- esrc/cachedrive.php
+++ esrc/cachedrive.php
@@ -44,8 +44,8 @@ require_once '../class/db.class.php';
 					<?php
 					//summary data
 					$db = new Database();
-					$start = date("Y-m-d H:i:s", strtotime('2017-05-05 20:00:00'));
-					$end = date("Y-m-d H:i:s", strtotime('2017-05-07 19:59:59'));
+					$start = date("Y-m-d H:i:s", strtotime('2017-06-04 00:00:00'));
+					$end = date("Y-m-d H:i:s", strtotime('2017-06-10 23:59:59'));
 					
 					//count of all actions performed in the specified period
 					$db->query("SELECT COUNT(*) as cnt
