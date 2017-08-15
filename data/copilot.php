c79a35d700a3d5ab810beaaba0463d6f2bd1f88c
Klaus Mueller <klmuell@web.de>
2017-08-15 19:39:36 +0200

    Update Copilot data provider
    - switch on of the session check (only allow authenticated requests)
    - add a new property to show 'system-located' status for the current system
    - prepare the rename of the system parameter from 'cache' to 'system'

--- data/copilot.php
+++ data/copilot.php
@@ -16,14 +16,20 @@ include_once '../class/caches.class.php';
 include_once '../class/rescue.class.php';
 
 try {
-	$cache = strtoupper ( $_REQUEST ["cache"] );
+	// try new parameter name "system"
+	$param = $_REQUEST ["system"];
+	if (!isset($param))
+	{
+		$param = $_REQUEST ["cache"];
+	}
+	$whSystem = strtoupper($param);
 	
 	session_start();
 	
 	if (session_status() != PHP_SESSION_ACTIVE || !isset($_SESSION['auth_copilot']))
 	{
 		$result['authorized'] = "Invalid session. Disabled or not allowed for copilot";
-//  		throw new Exception ( "Invalid session" );
+ 		throw new Exception("Invalid session");
 	}
 	else 
 	{
@@ -36,35 +42,50 @@ try {
 	$caches = new Caches($db);
 	$rescue = new Rescue($db);
 	
-	if ($cache && strlen ( $cache ) == 7) {
+	if ($whSystem && strlen($whSystem) == 7) {
 		// check for "No Sow" system
 		
-		if ($systems->validatename($cache) == 0) {
-			$row = $systems->getWHInfo($cache);
+		if ($systems->validatename($whSystem) == 0) {
+			$row = $systems->getWHInfo($whSystem);
 			// yes, add the system to the result
 			if (isset($row['Notes'])) {
 				$row['Notes'] = utf8_encode($row['Notes']);
 			}
 			$result ['system'] = $row;
 		} else {
-			throw new Exception ( 'Unknown system: ' . $cache );
+			throw new Exception ( 'Unknown system: ' . $whSystem );
 		}
 		
 		// we can sow here
 		// check for presence of a cache in system
-		$row = $caches->getCacheInfo($cache, TRUE);
+		$row = $caches->getCacheInfo($whSystem, TRUE);
 		$result ['cache'] = $row;
 		
 		// report only open SAR requests
-		$requests = $rescue->getSystemRequests($cache, 0);
+		$requests = $rescue->getSystemRequests($whSystem, 0);
 		$result ['rescue'] = count($requests);
-
+		
+		// initialize system located counter
+		$systemLocated = 0;
+		foreach ($requests as $req)
+		{
+			// check for located systems in open requests
+			if ($req['status'] === 'system-located')
+			{
+				// increment
+				$systemLocated++;
+				break;
+			}
+		}
+		// set the status to response
+		$result['located'] = $systemLocated;
+		
 		// check if tending is allowed for cache		
-		$result['tending'] = $caches->isTendingAllowed($cache);
-	} // $cache not present or wrong format
+		$result['tending'] = $caches->isTendingAllowed($whSystem);
+	} // $whSystem not present or wrong format
 	else
 	{
-		throw new Exception ( 'Invalid system: ' . $cache );
+		throw new Exception ( 'Invalid system: ' . $whSystem );
 	}
 } catch ( Exception $e ) {
     // map an error message to a response field
