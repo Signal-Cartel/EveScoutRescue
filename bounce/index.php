da321de97bd9101379988fc213d9257f47add5ac
ADP <adedparrot@gmail.com>
2020-06-28 08:25:49 -0400

    French Lang Bounce INstructions added

--- bounce/index.php
+++ bounce/index.php
@@ -12,15 +12,24 @@ include_once '../includes/auth-inc.php';
 $pgtitle = 'Bounce Method';
 include_once '../includes/head.php'; 
 ?>
+
+<style>
+.links a {
+	font-size: 1.4em;
+    color: rgba(255, 255, 255, 0.8);
+}
+</style>
+
 </head>
 
 <body>
 <div class="container">
 <div class="row" id="header" style="padding-top: 10px;">
 	<?php include_once '../includes/top-left.php'; ?>
-	<div class="col-sm-8 white" style="text-align: center; height: 100px; vertical-align: middle;">
+	<div class="col-sm-8 white links" style="text-align: center; height: 100px; vertical-align: middle;">
 		<br /><span class="sechead">Bounce Method Instructions</span><br /><br />
 		<a href="index_en.php">English</a><br />
+		<a href="index_fr.php">French</a><br />
 		<a href="index_zh.php">中文</a><br />
 		<a href="index_ja.php">日本語</a><br />
 		<a href="index_ko.php">한국어</a><br />
