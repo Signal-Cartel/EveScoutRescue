451327881a875ada9cc2fb5ccfe88e95c9800bd1
Thricehappy <thricehapus@gmail.com>
2020-08-30 11:20:40 -0400

    Storm Tracker

---
+++ class/storms.class.php
@@ -0,0 +1,95 @@
+<?php
+
+// check if called from an allowed page
+if (!defined('ESRC')) {
+	echo "Do not call the script direct!";
+	exit ( 1 );
+}
+
+
+class Storms
+{
+	var $db= null;
+	
+	public function __construct($database = NULL)
+	{
+		if (isset($database))
+		{
+			$this->db = $database;
+		}
+		else
+		{
+			// create a new database class instace
+			$this->connectDatabase ();
+		}
+    }
+    
+	
+	/**
+	 * Create a new DB connection.
+	 */
+	private function connectDatabase() {
+		$this->db = new Database ();
+	}
+	
+
+	/**
+	 * Create new entry into [storm_tracker] table
+	 * @param string $pilot Name of pilot submitting testimonial
+	 * @param bit $anon Anonymize display of testimonial; 1 = anonymous, 0 = not anonymous
+	 * @param string $method rescue method, either via rescue cache or search and rescue
+	 * @param string $testimonial Text of testimonial
+	 */
+	public function createStormEntry($evesystem, $pilot, $stormtype, $stormstrength)
+	{
+		$dateobserved = new DateTime('now', new DateTimeZone('UTC'));
+		
+		$this->db->beginTransaction();
+		$this->db->query("INSERT INTO storm_tracker (evesystem, pilot, stormtype, stormstrength, 
+								dateobserved)
+							VALUES (:evesystem, :pilot, :stormtype, :stormstrength, :dateobserved)");
+		$this->db->bind(":evesystem", $evesystem);
+		$this->db->bind(":pilot", $pilot);
+		$this->db->bind(":stormtype", $stormtype);
+		$this->db->bind(":stormstrength", $stormstrength);
+		$this->db->bind(":dateobserved", $dateobserved->format('Y-m-d H:i:s'));
+		$this->db->execute();
+		$this->db->endTransaction();
+	}
+    
+	
+	/**
+	 * Get list of storms.
+	 * @param string $last48hrs Get storm reports only from last 48 hours; default to true
+	 * @return array $result
+	 */
+	public function getStorms($last48hrs = true)
+	{
+        $where_clause = ($last48hrs === false) ? '' : 
+            'WHERE dateobserved BETWEEN DATE_SUB(NOW(), INTERVAL 2 DAY) AND NOW()';
+
+		$this->db->query("SELECT * FROM storm_tracker 
+							$where_clause
+							ORDER BY dateobserved DESC");
+        $result = $this->db->resultset();
+        $this->db->closeQuery();
+
+		return $result;
+	}
+
+
+	/**
+	 * Delete storm report entry from db
+	 * @param int $rowid ID of database row to delete
+	 */
+	public function removeStormEntry($rowid)
+	{
+		$this->db->beginTransaction();
+		$this->db->query("DELETE FROM storm_tracker WHERE id = :id");
+		$this->db->bind(':id', $rowid);
+		$this->db->execute();
+		$this->db->endTransaction();
+	}
+
+}
+?>
--- home/index.php
+++ home/index.php
@@ -8,6 +8,7 @@ $database = new Database();
 $caches = new Caches($database);
 $users = new Users($database);
 $rescues = new Rescue($database);
+$storms = new Storms($database);
 
 $pgtitle = "New Eden's Premier Wormhole Rescue Service";
 $rootCopilot = $_SERVER['DOCUMENT_ROOT'] . '/copilot/';
@@ -161,6 +162,112 @@ require '../page_templates/home_html-begin.php';
 		</div>
 	</div>
 </div>
+
+<!-- NEWS TICKER -->
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat');
+
+.onoffswitch3
+{
+    position: relative; 
+    -webkit-user-select:none; -moz-user-select:none; -ms-user-select: none;
+}
+
+.onoffswitch3-checkbox {
+    display: none;
+}
+
+.onoffswitch3-label {
+    display: block; overflow: hidden; cursor: pointer;
+    border: 0px solid #999999; border-radius: 0px;
+}
+
+.onoffswitch3-inner {
+    display: block; width: 200%; margin-left: -100%;
+    -moz-transition: margin 0.3s ease-in 0s; -webkit-transition: margin 0.3s ease-in 0s;
+    -o-transition: margin 0.3s ease-in 0s; transition: margin 0.3s ease-in 0s;
+}
+
+.onoffswitch3-inner > span {
+    display: block; float: left; position: relative; width: 50%; height: 30px; padding: 0; line-height: 30px;
+    font-size: 14px; color: white; font-family: 'Montserrat', sans-serif; font-weight: bold;
+    -moz-box-sizing: border-box; -webkit-box-sizing: border-box; box-sizing: border-box;
+}
+
+.onoffswitch3-inner .onoffswitch3-active {
+    padding-left: 10px;
+    background-color: #EEEEEE; color: #FFFFFF;
+}
+
+.onoffswitch3-inner .onoffswitch3-inactive {
+    width: 100px;
+    padding-left: 16px;
+    background-color: #EEEEEE; color: #FFFFFF;
+    text-align: right;
+}
+
+.onoffswitch3-switch {
+    display: block; width: 50%; margin: 0px; text-align: center; 
+    border: 0px solid #999999;border-radius: 0px; 
+    position: absolute; top: 0; bottom: 0;
+}
+.onoffswitch3-active .onoffswitch3-switch {
+    background: #27A1CA; left: 0;
+    width: 200px;
+}
+.onoffswitch3-inactive{
+    background: #A1A1A1; right: 0;
+    width: 20px;
+}
+.onoffswitch3-checkbox:checked + .onoffswitch3-label .onoffswitch3-inner {
+    margin-left: 0;
+}
+
+.glyphicon-remove{
+    padding: 3px 0px 0px 0px;
+    color: #fff;
+    background-color: #000;
+    height: 25px;
+    width: 25px;
+    border-radius: 15px;
+    border: 2px solid #fff;
+}
+
+.scroll-text{
+    color: #000;
+}
+</style>
+
+<div class="onoffswitch3">
+    <input type="checkbox" name="onoffswitch3" class="onoffswitch3-checkbox" id="myonoffswitch3" checked>
+    <label class="onoffswitch3-label" for="myonoffswitch3">
+        <span class="onoffswitch3-inner">
+            <span class="onoffswitch3-active">
+				<marquee class="scroll-text">
+				<?php
+				$rows = $storms->getStorms();
+				if (empty($rows)) {
+					echo 'No current reports <span class="glyphicon glyphicon-forward"></span> 
+						Stay tuned for our next update <span class="glyphicon glyphicon-forward"></span> ';
+				}
+				else {
+					foreach ($rows as $value) {
+						$dateobserved = date_create($value['dateobserved']);
+						echo $value['evesystem'] .' - '. $value['stormstrength'] .' ' 
+							.' Metaliminal '. $value['stormtype'] .' Ray Storm - last reported: '. 
+							date_format($dateobserved, "M-d@H:i");
+						echo ' <span class="glyphicon glyphicon-forward"></span> ';
+					}
+				}	?>
+				
+				</marquee>
+                <span class="onoffswitch3-switch">NEW EDEN WEATHER <span class="glyphicon glyphicon-remove"></span></span>
+            </span>
+            <span class="onoffswitch3-inactive"><span class="onoffswitch3-switch">SHOW WEATHER</span></span>
+        </span>
+    </label>
+</div>
+<!-- /NEWS TICKER -->
 
 <?php
 // HTML PAGE template - End
